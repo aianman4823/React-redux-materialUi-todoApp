bff076175d5e3ff17ed3f1a1ef23c47fabae1900
aianman4823 <akitoaianman1@gmail.com>
2019-06-30 01:37:03 +0900

    first commit

---
+++ src/actions/todos.js
@@ -0,0 +1,16 @@
+import {actionNameList} from '../const';
+
+
+export const addTodo = (todo) => ({
+    type: actionNameList.addTodo,
+    payload: {
+        todo
+    }
+});
+
+export const deleteTodo = (index) => ({
+    type: actionNameList.deleteTodo,
+    payload:{
+        id:index,
+    }
+});
